ac3e6ba8911a5a6f93ea125ffb9fd2d3d2ca2b9b
Victor Velasquez <VictorVelasquez@protonmail.com>
2023-04-25 10:25:08 +0200

    Ajout des réponses aux questions dans les fichiers json

--- docu-form/src/data/QuestionsCompetence.js
+++ docu-form/src/data/QuestionsCompetence.js
@@ -1,38 +1,83 @@
 export const questionsCompetence = [
   {
     question: 'Formations pour Apprendre à apprendre, changement de paradigme, "être à la page" (au-delà des compétences "justes" nécessaires) ?',
+    answers : {
+      0: "Désintérêt pour l'amélioration de la compétence des équipes",
+      1: "Nombreuses formations métiers dans le plan de formation",
+      2: "Possibilité de choisir et de suivre des formations \"annexes\" au métier et/ou temps dédié pour de la veille ou de l'auto-formation",
+    },
     id: '0',
   },
   {
     question: 'Le co-développement (outil d\'intelligence collective) existe-t-il dans l\'entreprise ?',
+    answers : {
+      0: "Pas du tout",
+      1: "Quelques initiatives",
+      2: "Démarche instaurée et pratiquée régulièrement",
+    },
     id: '1',
   },
   {
     question: 'Les collaborateurs sont-ils amenés à partager leurs compétences et sous quelles formes ?',
+    answers : {
+      0: "Aucune initiative",
+      1: "Capitalisation d'un certain nombre de savoirs",
+      2: "Véritable communauté de pratiques mise en place",
+    },
     id: '2',
   },
   {
     question: 'Le mentoring (fonctionnement en binôme) existe-t-il pour assurer le transfert de compétences ?',
+    answers : {
+      0: "Aucune initiative",
+      1: "Quelques initiatives",
+      2: "Systématiquement lors d'une prise de poste",
+    },
     id: '3',
   },
   {
     question: 'Les managers sont-ils aussi formateurs sur certains sujet pour l\'entreprise entière ?',
+    answers : {
+      0: "Non",
+      1: "Quelques fois",
+      2: "Quasi systématiquement",
+    },
     id: '4',
   },
   {
-    question: 'L\'entreprise favorise-t-elle l\'excellence technique ? (Principe 9 du Manifeste Agile) ?'
+    question: 'L\'entreprise favorise-t-elle l\'excellence technique ? (Principe 9 du Manifeste Agile) ?',
+    answers : {
+      0: "Pas du tout",
+      1: "Oui un peu",
+      2: "Oui",
+    },
     id: '5',
   },
   {
     question: 'Déployez vous les pratiques du \'Faire Agile\', c\'est-à-dire une ou plusieurs des "méthodes" agiles ?',
+    answers : {
+      0: "NON, nous les projets sont tous en cycle en V / cascade",
+      1: "Quelques projets pilotes",
+      2: "Déploiement systématique d'une ou plusieurs méthodes",
+    },
     id: '6',
   },
   {
     question: 'Votre entreprise promeut-elle un "état d\'esprit agile" ?',
+    answers : {
+      0: "Non",
+      1: "Quelques valeurs agiles promues",
+      2: "Oui",
+    },
     id: '7',
   },
   {
     question: 'Votre entreprise gère-t-elle humainement les compétences ?',
+    answers : {
+      0: "Non",
+      1: "Un peu",
+      2: "Oui",
+    },
     id: '8',
   },
 ];
--- docu-form/src/data/QuestionsNumerique.js
+++ docu-form/src/data/QuestionsNumerique.js
@@ -1,83 +1,182 @@
 export const questionsNumerique = [
   {
     question: 'Votre entreprise dégage t-elle une part de CA  par des produits ou services en ligne ?',
+    answers : {
+      0: "Moins de 10%",
+      1: "Oui environ 20%",
+      2: "Oui environ 30%",
+    },
     id: '0',
   },
   {
     question: 'La mise en place d’outils numériques a-t-elle permis d\'optimiser les coûts, les délais et la qualité ?',
+    answers : {
+      0: "Aucun",
+      1: "Oui mais pas pour tous les critères",
+      2: "Oui pour tous les critères ",
+    },
     id: '1',
   },
   {
     question: 'Comment les outils sont-ils intégrés dans les process de l’entreprise ?',
+    answers : {
+      0: "Absence d'outils ou de procédures, manque d'utilité",
+      1: "En partie, les outils ne sont pas toujours adaptés à l'activité et pas toujours rattachés à des procédures, sans entraver ses principales fonctions",
+      2: "A part entière, il existe des procédures et formations aux outils, ces outils sont intituitifs et adaptés à l'activité",
+    },
     id: '2',
   },
   {
     question: 'Comment partagez-vous les données numériques avec les parties prenantes (clients, fournisseurs,…) ?',
+    answers : {
+      0: "En mode novice : au mieux par mail ou via le site internet",
+      1: "En mode collaboratif : quelques aménagements (type sharepoint, drive) pour des sujets plutôt ponctuels",
+      2: "En mode avancé : des plateformes dédiées (type extranet, applications) sont intégrées au mode de partage permanent",
+    },
     id: '3',
   },
   {
     question: 'Mesurez-vous les impacts du numérique sur votre entreprise ?',
+    answers : {
+      0: "Non",
+      1: "Oui et non on observe sans mesurer",
+      2: "Oui des outils de mesure sont en place",
+    },
     id: '4',
   },
   {
     question: 'Quel est l’impact du numérique sur la démarche RSE de l’entreprise ?',
+    answers : {
+      0: "Absence de démarche RSE",
+      1: "C'est un sujet observé de loin, on traite les questions d'obligation légales sans aller plus loin",
+      2: "Le numérique est un sujet totalement rattaché à la démarche RSE",
+    },
     id: '5',
   },
   {
     question: 'Existe-t-il des freins (stratégiques, financiers,…) à l’ investissement dans les outils numériques ?',
+    answers : {
+      0: "Oui la statégie de l'entreprise n'est pas orientée en faveur du numérique, elle n'investit pas",
+      1: "Oui l'entreprise est plutôt frileuse, on ne dépense que le strict nécessaire pour que les outils fonctionnent sans recherche de performance",
+      2: "Non des budgets sont alloués et l'entreprise a bien compris cette nécessité dans sa stratégie",
+    },
     id: '6',
   },
   {
     question: 'L’entreprise dispose-t-elle : d’un site internet, d’un compte LinkedIn, d’une page Facebook, d’un compte Twitter,... ?',
+    answers : {
+      0: "Non le site/la page réseau sont inexistants ou inactifs",
+      1: "Oui mais seulement sur le site internet",
+      2: "Oui la présence de l'entreprise sur les réseaux est visible, elle correspond au secteur d'activité",
+    },
     id: '7',
   },
   {
     question: 'Comment utilisez-vous le numérique dans la relation client ? (nouveau modèle de vente, nouveau modèle d’échanges avec les clients, communauté, story, chat,...)?',
-    reponse 1:
+    answers : {
+      0: "Absence de plateforme de contact",
+      1: "Des accès dédiés pour les échanges entre le client et l'entreprise sont existent, le suivi des contacts n'est pas optimal",
+      2: "Plusieurs modes d'utilisation sont possibles pour échanger avec l'externe, ils sont performants et adaptés avec l'activité de l'entreprise",
+    },
     id: '8',
   },
   {
     question: 'L’entreprise dispose-t-elle d’un community manager ?',
+    answers : {
+      0: "Non aucune personne n'est dédiée ",
+      1: "Non mais une personne se charge de l'animation des réseaux sociaux parmi ses autres tâches",
+      2: "Oui",
+    },
     id: '9',
   },
   {
     question: 'Certains de vos collaborateurs sont-ils actifs sur les réseaux sociaux au nom de l’entreprise ?',
+    answers : {
+      0: "Non ou rarement",
+      1: "Oui sans incitation",
+      2: "Oui avec des incitations de l'entreprise pour le faire",
+    },
     id: '10',
   },
   {
     question: 'Comment mesurez-vous et exploitez-vous les données issues du site de votre entreprise ?',
+    answers : {
+      0: "Absence de suivi",
+      1: "Des indicateurs sont en place mais plutôt de façon passive, ils servent à mesurer ",
+      2: "Des indicateurs sont mis en place afin de mesurer l'efficacité et l'impact du site sur les réseaux, une recherche de performance est en place pour toucher toujours plus et mieux",
+    },
     id: '11',
   },
   {
     question: 'Avez-vous observé chez vos concurrents des pratiques innovantes ?',
+    answers : {
+      0: "Oui ils sont tellement performants et en avance que nous pourrions perdre des marchés",
+      1: "Oui ils ont quelques belles idées, si seulement on pouvait faire aussi bien qu'eux",
+      2: "Oui elles servent comme base de reflexion et de développement pour notre propre entreprise. On peut être rivaliser !",
+    },
     id: '12',
   },
   {
     question: 'Vos collaborateurs sont-ils équipés de nouveaux équipements numériques de travail (PC portable, tablette, smartphone,…) ?',
+    answers : {
+      0: "Les fonctions supports ne sont pas amenées à être équipé pour le travail à distance. L'entreprise fonctionne majoritairement sur le mode papier.",
+      1: "Les processus s'appuient rarement sur la numérisation des données et les outils numériques. Le potentiel ne semble pas pleinement exploité.",
+      2: "Les processus de travail s'appuient sur des outils numériques quand ceux-ci sont possibles. L'ensemble des fonctions de l'entreprise sont équipées pour pouvoir réaliser le travail à distance quand celui-ci est possible",
+    },
     id: '13',
   },
   {
     question: 'L’arrivée des outils digitaux a-t-elle eu un impact sur les pratiques et la culture de l’entreprise ?',
+    answers : {
+      0: "Non au contraire, peu de monde y adhère",
+      1: "Oui dans l'ensemble toutefois nous rencontrons parfois des contraintes (pratiques, techniques,…)",
+      2: "Oui gain de temps, facilité, économies et fiabilité",
+    },
     id: '14',
   },
   {
     question: 'Comment vous êtes-vous approprié et comment avez-vous accompagné ces évolutions?',
+    answers : {
+      0: "Absence de communication et d'accompagnement",
+      1: "La communication n'est pas parfaite, les personnes qui peuvent se sentir en difficultés avec les nouvelles pratiques ne sont pas forcémment considérées.",
+      2: "Tout est en place pour faciliter la compréhension et l'adhésion aux nouveaux outils/pratiques",
+    },
     id: '15',
   },
   {
     question: 'Les nouvelles possibilités technologiques ont-elles fait évoluer le modèle d’organisation de l’entreprise et/ou votre management, vers plus de collaboration avec des acteurs internes ou externes ?',
+    answers : {
+      0: "L'organisation est en silot, absence de projets transversaux et d'interactions avec les autres sites",
+      1: "Oui quelques évolutions s'opèrent pour travailler en inter-service ou avec l'externe, toutefois le potentiel n'est pas pleinement exploité",
+      2: "Oui on sort complétement du mode silot et on favorise le mode projet",
+    },
     id: '16',
   },
   {
     question: 'Mobilisez-vous des outils de veille et mettez-vous en œuvre des modalités de curation et de partage de l’ information ?',
+    answers : {
+      0: "Absence de veille",
+      1: "Il y a un peu de benchmark toutefois les décisions d'évolutions sont un peu tardive (quand il n y a plus le choix)",
+      2: "Oui l'entreprise est en capacité de mesurer sa performance et de réagir rapidement pour se mettre à jour",
+    },
     id: '17',
   },
   {
     question: 'Les fonctionnalités des outils sont-elles augmentées par la pratique de vos collaborateurs ?',
+    answers : {
+      0: "Absence de prise en compte des pratiques terrain",
+      1: "Oui mais les outils sont vieillissants, il y a des freins pour les faire évoluer",
+      2: "Oui totalement les outils s'adaptent au terrain quotidiennement",
+    },
     id: '18',
   },
   {
     question: 'Comment accompagnez-vous vos collaborateurs dans la transition numérique ?',
+    answers : {
+      0: "Absence d'accompagnement",
+      1: "L'entreprise accompagne en faisant de son mieux pour faire adhérer les autres, il y a parfois quelques blocages. J'aimerais parfois être moi-même accompagné",
+      2: "L'entreprise est pro-active par la promotion des innovations et des outils numériques.",
+    },
     id: '19',
   },
 ];
