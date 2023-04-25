c9706026548b1160dafe6f3cb3d759da98e7d91e
Victor Velasquez <VictorVelasquez@protonmail.com>
2023-04-25 09:50:21 +0200

    Mise à jour des fichiers json contenant les questions

--- docu-form/src/data/QuestionsNumerique.js
+++ docu-form/src/data/QuestionsNumerique.js
@@ -1,5 +1,83 @@
 export const questionsNumerique = [
-
-
-
+  {
+    question: 'Votre entreprise dégage t-elle une part de CA  par des produits ou services en ligne ?',
+    id: '0',
+  },
+  {
+    question: 'La mise en place d’outils numériques a-t-elle permis d\'optimiser les coûts, les délais et la qualité ?',
+    id: '1',
+  },
+  {
+    question: 'Comment les outils sont-ils intégrés dans les process de l’entreprise ?',
+    id: '2',
+  },
+  {
+    question: 'Comment partagez-vous les données numériques avec les parties prenantes (clients, fournisseurs,…) ?',
+    id: '3',
+  },
+  {
+    question: 'Mesurez-vous les impacts du numérique sur votre entreprise ?',
+    id: '4',
+  },
+  {
+    question: 'Quel est l’impact du numérique sur la démarche RSE de l’entreprise ?',
+    id: '5',
+  },
+  {
+    question: 'Existe-t-il des freins (stratégiques, financiers,…) à l’ investissement dans les outils numériques ?',
+    id: '6',
+  },
+  {
+    question: 'L’entreprise dispose-t-elle : d’un site internet, d’un compte LinkedIn, d’une page Facebook, d’un compte Twitter,... ?',
+    id: '7',
+  },
+  {
+    question: 'Comment utilisez-vous le numérique dans la relation client ? (nouveau modèle de vente, nouveau modèle d’échanges avec les clients, communauté, story, chat,...)?',
+    reponse 1:
+    id: '8',
+  },
+  {
+    question: 'L’entreprise dispose-t-elle d’un community manager ?',
+    id: '9',
+  },
+  {
+    question: 'Certains de vos collaborateurs sont-ils actifs sur les réseaux sociaux au nom de l’entreprise ?',
+    id: '10',
+  },
+  {
+    question: 'Comment mesurez-vous et exploitez-vous les données issues du site de votre entreprise ?',
+    id: '11',
+  },
+  {
+    question: 'Avez-vous observé chez vos concurrents des pratiques innovantes ?',
+    id: '12',
+  },
+  {
+    question: 'Vos collaborateurs sont-ils équipés de nouveaux équipements numériques de travail (PC portable, tablette, smartphone,…) ?',
+    id: '13',
+  },
+  {
+    question: 'L’arrivée des outils digitaux a-t-elle eu un impact sur les pratiques et la culture de l’entreprise ?',
+    id: '14',
+  },
+  {
+    question: 'Comment vous êtes-vous approprié et comment avez-vous accompagné ces évolutions?',
+    id: '15',
+  },
+  {
+    question: 'Les nouvelles possibilités technologiques ont-elles fait évoluer le modèle d’organisation de l’entreprise et/ou votre management, vers plus de collaboration avec des acteurs internes ou externes ?',
+    id: '16',
+  },
+  {
+    question: 'Mobilisez-vous des outils de veille et mettez-vous en œuvre des modalités de curation et de partage de l’ information ?',
+    id: '17',
+  },
+  {
+    question: 'Les fonctionnalités des outils sont-elles augmentées par la pratique de vos collaborateurs ?',
+    id: '18',
+  },
+  {
+    question: 'Comment accompagnez-vous vos collaborateurs dans la transition numérique ?',
+    id: '19',
+  },
 ];
---
+++ docu-form/src/data/QuestionsReactivite.js
@@ -0,0 +1,46 @@
+export const questionsReactivite = [
+  {
+    question: 'Valeur supérieure utilisable livrée plus tôt (Fonction principale utilisable dès les premières versions)',
+    id: '0',
+  },
+  {
+    question: 'Réactivité face aux impératifs prépondérants',
+    id: '1',
+  },
+  {
+    question: 'Mesure de la vélocité de l\'équipe (indicateur de suivi du travail d\'une équipe de conception)',
+    id: '2',
+  },
+  {
+    question: 'Les installations techniques et de gestion sont modernes (TIC/ERP/CRM)',
+    id: '3',
+  },
+  {
+    question: 'Les équipes sont en capacité d\'autonomiser une partie de leurs tâches',
+    id: '4',
+  },
+  {
+    question: 'Les équipes s’inscrivent dans un cadre Agile Lean',
+    id: '5',
+  },
+  {
+    question: 'Les mécanismes de livraison et de synchronisation sont matures',
+    id: '6',
+  },
+  {
+    question: 'Les innovations produit tiennent compte de l\'urgence climatique',
+    id: '7',
+  },
+  {
+    question: 'Les processus internes sont remis en cause pour diminuer leur impact environnemental\',
+    id: '8',
+  },
+  {
+    question: 'Les parties prenantes sont sélectionnées en fonction de leur éthique vis-à-vis du développement durable',
+    id: '9',
+  },
+  {
+    question: 'Veille stratégique',
+    id: '10',
+  },
+];
