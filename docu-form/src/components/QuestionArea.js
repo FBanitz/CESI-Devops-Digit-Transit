8b7667cf14b8d4d63cd6696c39dedf185b6deffa
Victor Velasquez <VictorVelasquez@protonmail.com>
2023-04-25 16:42:19 +0200

    Ajout d'un élément permettant d'afficher les questions

---
+++ docu-form/src/components/QuestionArea.js
@@ -0,0 +1,10 @@
+import "../styles/questions.css"
+
+function QuestionArea() {
+    return (
+        <div className="questionArea">
+        </div>
+    );
+}
+
+export default QuestionArea
--- docu-form/src/pages/Questions.js
+++ docu-form/src/pages/Questions.js
@@ -1,13 +1,15 @@
 import Banner from "../components/Banner.js"
 import Footer from "../components/Footer.js"
+import QuestionArea from "../components/QuestionArea"
+
 import "../styles/questions.css"
 
 function Questions(){
     return (
         <div className="flex flex-col">
           <Banner />
-          <div className="">
-          </div>
+
+          <QuestionArea />
           <Footer />
         </div>
     );
