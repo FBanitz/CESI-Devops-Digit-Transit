d5bd281d26ef672a2fa5767167e80ce100a166ad
Victor Velasquez <VictorVelasquez@protonmail.com>
2023-04-25 09:27:48 +0200

    Premier ajout des questions dans un fichier json

---
+++ docu-form/src/data/QuestionsCompetence.js
@@ -0,0 +1,38 @@
+export const questionsCompetence = [
+  {
+    question: "Formations pour Apprendre à apprendre, changement de paradigme, \"être à la page\" (au-delà des compétences \"justes\" nécessaires) ?",
+    id: "0",
+  },
+  {
+    question: "Le co-développement (outil d'intelligence collective) existe-t-il dans l'entreprise ?",
+    id: "1",
+  },
+  {
+    question: "Les collaborateurs sont-ils amenés à partager leurs compétences et sous quelles formes ?",
+    id: "2",
+  },
+  {
+    question: "Le mentoring (fonctionnement en binôme) existe-t-il pour assurer le transfert de compétences ?",
+    id: "3",
+  },
+  {
+    question: "Les managers sont-ils aussi formateurs sur certains sujet pour l'entreprise entière ?",
+    id: "4",
+  },
+  {
+    question: "L'entreprise favorise-t-elle l'excellence technique ? (Principe 9 du Manifeste Agile) ?"
+    id: "5",
+  },
+  {
+    question: "Déployez vous les pratiques du "Faire Agile", c'est-à-dire une ou plusieurs des \"méthodes\" agiles ?",
+    id: "6",
+  },
+  {
+    question: "Votre entreprise promeut-elle un \"état d'esprit agile\" ?",
+    id: "7",
+  },
+  {
+    question: "Votre entreprise gère-t-elle humainement les compétences ?",
+    id: "8",
+  }
+];
