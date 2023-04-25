2b2dd63716c304707cdf44c56f39da9372c247b1
François BANITZ <banitz.francois@gmail.com>
2023-04-25 10:15:52 +0200

    pages competences, numerique and reactivite now read data from files

--- docu-form/src/components/Numerique.js
+++ docu-form/src/components/Numerique.js
@@ -1,50 +1,14 @@
+import { questionsNumerique } from "../data/QuestionsNumerique";
+
 function Numerique() {
     return (
         <div>
-            <h1>Axe Numérique</h1>
-
-
-            <p>Votre entreprise dégage t-elle une part de CA  par des produits ou services en ligne ?</p>
-            <p>Score : 0</p>
-            <p>La mise en place d’outils numériques a-t-elle permis d'optimiser les coûts, les délais et la qualité ?</p>
-            <p>Score : 2</p>
-            <p>Comment les outils sont-ils intégrés dans les process de l’entreprise ?</p>
-            <p>Score : 2</p>
-            <p>Comment partagez-vous les données numériques avec les parties prenantes (clients, fournisseurs,…) ?</p>
-            <p>Score : 2</p>
-            <p>Mesurez-vous les impacts du numérique sur votre entreprise ?</p>
-            <p>Score : 1</p>
-            <p>Quel est l’impact du numérique sur la démarche RSE de l’entreprise ?</p>
-            <p>Score : 1</p>
-            <p>Existe-t-il des freins (stratégiques, financiers,…) à l’ investissement dans les outils numériques ?</p>
-            <p>Score : 2</p>
-            <p>L’entreprise dispose-t-elle : d’un site internet, d’un compte LinkedIn, d’une page Facebook, d’un compte Twitter,... ?</p>
-            <p>Score : 2</p>
-            <p>Comment utilisez-vous le numérique dans la relation client ? (nouveau modèle de vente, nouveau modèle d’échanges avec les clients, communauté, story, chat,...)?</p>
-            <p>Score : 2</p>
-            <p>L’entreprise dispose-t-elle d’un community manager ?</p>
-            <p>Score : 1</p>
-            <p>Certains de vos collaborateurs sont-ils actifs sur les réseaux sociaux au nom de l’entreprise ?</p>
-            <p>Score : 0</p>
-            <p>Comment mesurez-vous et exploitez-vous les données issues du site de votre entreprise ?</p>
-            <p>Score : 1</p>
-            <p>Avez-vous observé chez vos concurrents des pratiques innovantes ?</p>
-            <p>Score : 2</p>
-            <p>Vos collaborateurs sont-ils équipés de nouveaux équipements numériques de travail (PC portable, tablette, smartphone,…) ?</p>
-            <p>Score : 2</p>
-            <p>L’arrivée des outils digitaux a-t-elle eu un impact sur les pratiques et la culture de l’entreprise ?</p>
-            <p>Score : 2</p>
-            <p>Comment vous êtes-vous approprié et comment avez-vous accompagné ces évolutions?</p>
-            <p>Score : 2</p>
-            <p>Les nouvelles possibilités technologiques ont-elles fait évoluer le modèle d’organisation de l’entreprise et/ou votre management, vers plus de collaboration avec des acteurs internes ou externes ?</p>
-            <p>Score : 2</p>
-            <p>Mobilisez-vous des outils de veille et mettez-vous en œuvre des modalités de curation et de partage de l’ information ?</p>
-            <p>Score : 1</p>
-            <p>Les fonctionnalités des outils sont-elles augmentées par la pratique de vos collaborateurs ?</p>
-            <p>Score : 2</p>
-            <p>Comment accompagnez-vous vos collaborateurs dans la transition numérique ?</p>
-            <p>Score : 2</p>
-
+            <h1>Axe Compétence</h1>
+            {questionsNumerique.map((question) => (
+                <div key={question.id}>
+                    <p>{question.question}</p>
+                </div>
+            ))}
         </div>
     );
 }
