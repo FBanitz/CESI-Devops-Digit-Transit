58d125eff9fac34b0b2ce8c6a165e9cef630a1ec
Victor Velasquez <VictorVelasquez@protonmail.com>
2023-04-27 10:35:11 +0200

    Ajout d'un numéro de version dans le pied de page

--- docu-form/src/components/Footer.js
+++ docu-form/src/components/Footer.js
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 function Footer(){
     return (
         <div className="footer flex justify-center p-2 text-center bg-zinc-950">
-          <div className="flex justify-center p-2 text-center flex-col">
-            <p className="text-cyan-50"><Link to="https://github.com/FBanitz/CESI-Devops-Digit-Transit" target="_blank">Github</Link></p>
+          <div className="flex justify-center text-center flex-col">
+            <p className="text-cyan-50 text-lg"><Link to="https://github.com/FBanitz/CESI-Devops-Digit-Transit" target="_blank">Github</Link></p>
+            <p className="text-cyan-50 text-sm">Version 1.1.1</p>
           </div>
         </div>
     );
