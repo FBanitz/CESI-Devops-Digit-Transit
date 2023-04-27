812037eebe5083c1c3395ba831aeec2b03e54ae3
François BANITZ <banitz.francois@gmail.com>
2023-04-27 13:55:04 +0200

    added questions form
    it is now possible to see each question and its answers individually and skip each of them to see the next one

--- docu-form/src/index.js
+++ docu-form/src/index.js
@@ -5,12 +5,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./styles/index.css"
 
 import Home from './pages/Home'
-import Questions from './pages/Questions'
 
-import Competences from './components/Competences'
-import Reactivite from './components/Reactivite'
-import Numerique from './components/Numerique'
+import QuestionList from './pages/QuestionList'
 import Synthese from './components/Synthese'
+import QuestionForm from './pages/QuestionForm';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,13 +16,13 @@ root.render(
     <Router>
       <Routes>
         {/* <Route path="/" element={<Home />}></Route> */}
-        <Route path="/competences" element={<Competences />}></Route>
-        <Route path="/reactivite" element={<Reactivite />}></Route>
-        <Route path="/numerique" element={<Numerique />}></Route>
+        <Route path="/competences" element={<QuestionList axisId='1' />}></Route>
+        <Route path="/numerique" element={<QuestionList axisId='2' />}></Route>
+        <Route path="/reactivite" element={<QuestionList axisId='3' />}></Route>
         {/* <Route path="/synthese" element={<Synthese />}></Route> */}
         <Route path="/synthese" element={<Synthese />}></Route>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/questions" element={<Questions />}></Route>
+        <Route path="/questions" element={<QuestionForm />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
---
+++ docu-form/src/pages/QuestionList.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import "../styles/rapport.css"
+
+class QuestionList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            axisId: props.axisId,
+            axis: null,
+            questions: null
+        };
+    }
+
+    componentDidMount() {
+
+        let axisId = this.state.axisId;
+
+        let questionEndpoint = 'http://localhost:8055/items/question';
+
+        if (axisId) {
+            let axisEndpoint = 'http://localhost:8055/items/axis/' + axisId;
+
+            fetch(
+                axisEndpoint, {
+                "method": 'GET',
+                "headers": {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            },
+            )
+                .then(res => res.json())
+                .then((json) => {
+                    this.setState({ axis: json.data })
+
+                        // (json.data)
+                        (this.state.axis)
+                });
+
+
+            questionEndpoint += '?filter={"axis_id": {"_eq": ' + axisId + '}}';
+
+        }
+
+        fetch(
+            questionEndpoint, {
+            "method": 'GET',
+            "headers": {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+
+            },
+        },
+        )
+            .then(res => res.json())
+            .then((json) => {
+                this.setState({ questions: json.data })
+                    (this.state.questions)
+            })
+
+    }
+
+
+    render() {
+        const { questions } = this.state;
+        const { axis } = this.state;
+        return (
+            <div>
+                {axis ? <h1 className="text-zinc-50 text-2xl">Questions axe {axis.name}</h1> : <h1 className="text-zinc-50 text-2xl">Questions</h1>}
+                {questions ?
+                    questions.map((question) => (
+                        <div key={question.id}>
+                            <p className="text-zinc-50">{question.question}</p>
+                        </div>
+                    )) : null
+                }
+            </div>
+        )
+    }
+}
+
+export default QuestionList
